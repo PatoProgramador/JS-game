85d92bef520ee1b91f7dbd4b1725a6a8a1074156
PatoProgramador <skpates18@gmail.com>
2024-01-22 17:04:15 -0500

    Add Else and comments

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// variables
 let secretNumber = 6;
-let userNum = prompt('Indica un número, por favor: ');
-
+let userNum = prompt('Indica un número entre 1 y 10, por favor: ');
+// comparacion
 if (userNum == secretNumber) {
-    alert('¡Acertaste el número!')
+    // si el numero es igual
+    alert('¡Acertaste el número!');
+} else {
+    // si no acierta el numero
+    alert('Lo siento, no acertaste el número');
 }
