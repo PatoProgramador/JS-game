4f476b55562ada231618b9530a76a1cc58ccdb31
PatoProgramador <skpates18@gmail.com>
2024-01-22 17:55:02 -0500

    Add while loop

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 // variables
 let secretNumber = 8;
-let userNumber = prompt('Indica un número entre 1 y 10, por favor: ');
-// comparacion
-if (userNumber == secretNumber) {
-    // si el numero es igual
-    alert(`¡Acertaste el número! el número es: ${userNumber}`);
-} else {
-    if (userNumber > secretNumber) {
-        alert('El número secreto es menor');
+let userNumber = 0;
+
+while (userNumber != secretNumber) {
+    userNumber = prompt('Indica un número entre 1 y 10, por favor: ');
+
+    // comparacion
+    if (userNumber == secretNumber) {
+        // si el numero es igual
+        alert(`¡Acertaste el número! el número es: ${userNumber}`);
     } else {
-        alert('El número secreto es mayor');
+        // condicionales anidados
+        if (userNumber > secretNumber) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
     }
-    // si no acierta el numero
-    // alert('Lo siento, no acertaste el número');
 }
