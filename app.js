43d352957620faa827d77670a116b0d9bf271b7d
PatoProgramador <skpates18@gmail.com>
2024-01-23 17:25:32 -0500

    Add function to generate a random number

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+// variables
+let secretNum = generateRandomNum();
+// asignar texto a elementos html
 function assignTextElement(elemento, texto) {
     let htmlElement = document.querySelector(elemento);
     htmlElement.innerHTML = texto;
+    return;
+}
+// generador de numero aleatorio
+function generateRandomNum() {
+    return Math.floor(Math.random() * 10) + 1;
 }
 
 function userAttempt() {
     alert('Click jejejeje');
+    return;
 }
 
 assignTextElement('h1', 'Juego del número secreto');
