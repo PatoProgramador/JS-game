5abea3e36f3137bdd3f36a1a19907481236fb49b
PatoProgramador <skpates18@gmail.com>
2024-01-23 18:04:47 -0500

    Add the verify number function

--- app.js
+++ app.js
@@ -10,11 +10,21 @@ function assignTextElement(elemento, texto) {
 function generateRandomNum() {
     return Math.floor(Math.random() * 10) + 1;
 }
+// funcion que compara el numero del input con el random
+function verifyAttempt() {
+    let userNum = parseInt(document.getElementById('userValue').value);
 
-function userAttempt() {
-    alert('Click jejejeje');
+    if (userNum === secretNum) {
+        assignTextElement('p', '¡Acertaste el número!');
+    } else {
+        if (userNum > secretNum) {
+            assignTextElement('p', 'El número secreto es menor');
+        } else {
+            assignTextElement('p', 'El número secreto es mayor');
+        }
+    }
     return;
 }
 
 assignTextElement('h1', 'Juego del número secreto');
-assignTextElement('p', 'Indica un número del 1 al 100');
+assignTextElement('p', 'Indica un número entre 1 al 100');
