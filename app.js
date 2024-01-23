3adf75500c3d391226386116be024c3c3ad99b69
PatoProgramador <skpates18@gmail.com>
2024-01-23 18:17:36 -0500

    Add function to wish the input box

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // variables
 let secretNum = generateRandomNum();
+let attempts = 1;
+// funciones
 // asignar texto a elementos html
 function assignTextElement(elemento, texto) {
     let htmlElement = document.querySelector(elemento);
@@ -10,21 +12,29 @@ function assignTextElement(elemento, texto) {
 function generateRandomNum() {
     return Math.floor(Math.random() * 10) + 1;
 }
+// limpiar la caja
+function newBox() {
+    document.querySelector('#userValue').value = '';
+}
 // funcion que compara el numero del input con el random
 function verifyAttempt() {
     let userNum = parseInt(document.getElementById('userValue').value);
 
+    // en caso de acertar
     if (userNum === secretNum) {
-        assignTextElement('p', '¡Acertaste el número!');
+        assignTextElement('p', `Acertaste el número en ${attempts} ${attempts === 1 ? 'intento' : 'intentos'}`);
     } else {
+        // caso donde no se acierta
         if (userNum > secretNum) {
             assignTextElement('p', 'El número secreto es menor');
         } else {
             assignTextElement('p', 'El número secreto es mayor');
         }
+        attempts++;
+        newBox();
     }
     return;
 }
-
+// ejecuciones necesarias
 assignTextElement('h1', 'Juego del número secreto');
 assignTextElement('p', 'Indica un número entre 1 al 100');
